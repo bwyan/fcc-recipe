e1653538cdc0755054e440bf4211525ed923242e
Bryan Wegman <xxbw@icloud.com>
2017-02-09 20:42:56 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Recipe Manager
+
+A react.js project for Free Code Camp.
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+//styles
+import './stylesheets/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Recipes</h1>
+        <p>App <span>Goes</span> Here</p>
+      </div>
+    )
+  }
+}
+
+export default App;
