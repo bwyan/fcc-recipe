e6bc22f3dc7acacc55965aa6ab11dad77fc4cf27
Bryan Wegman <xxbw@icloud.com>
2017-02-10 20:23:00 -0800

    Add Recipe component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, {Component} from 'react';
 import Header from './components/Header.js';
 import RecipeList from './components/RecipeList.js';
+import Recipe from './components/Recipe.js';
 
 //styles
 import './App.scss';
 
 class App extends Component {
+  constructor() {
+  	super();
+
+  	this.state = {
+  		recipes: [],
+
+  	}
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
         <main>
         	<RecipeList />
+        	<Recipe />
         </main>
       </div>
     )
---
+++ src/components/Recipe.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Recipe extends React.Component {
+	render() {
+		return(
+			<div>
+				<h2>Recipe Name</h2>
+				<ol>
+					<li>Water</li>
+					<li>Coffee beans</li>
+				</ol>
+			</div>
+		)
+	}
+}
+
+export default Recipe;
