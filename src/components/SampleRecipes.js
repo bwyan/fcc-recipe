3e4e13d9fb2c30c630d64f7e950c45f0631145a0
Bryan Wegman <xxbw@icloud.com>
2017-02-18 11:39:53 -0800

    Add SampleRecipes to display defaults when a user is starting fresh.

---
+++ src/components/SampleRecipes.js
@@ -0,0 +1,16 @@
+const SampleRecipes = {
+	'recipe-1': {
+		name: 'Dry Martini',
+		ingredients: ['3 Oz. Dry Gin', '1 Oz. Dry Vermouth',  '2 Dashes of Orange Bitters (optional)', 'Garnish: Olive or Lemon twist']
+	},
+	'recipe-2': {
+		name: 'Manhattan',
+		ingredients: ['2 Oz. Rye Whiskey', '1 Oz. Sweet Vermouth',  '2 Dashes of Angostura Bitters', 'Garnish: Luxardo Cherry']
+	},
+	'recipe-3': {
+		name: 'Margarita',
+		ingredients: ['2 Oz. Tequila Blanco', '1 Oz. Triple Sec or Cointreau',  '3/4 Oz. Lime Juice', '1/4 Oz. Agave or Simple Syrup (optional)', 'Garnishes: Salt Rim and Lime Wedge']
+	},
+}
+
+export default SampleRecipes;
