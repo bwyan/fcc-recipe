4dab0d5447ca06bd51c8561dcf8f2e3982da0fd2
Bryan Wegman <xxbw@icloud.com>
2017-02-28 20:22:41 -0800

    Add more  recipes.

--- src/components/SampleRecipes.js
+++ src/components/SampleRecipes.js
@@ -11,6 +11,27 @@ const SampleRecipes = {
 		name: 'Margarita',
 		ingredients: ['2 Oz. Tequila Blanco', '1 Oz. Triple Sec or Cointreau',  '3/4 Oz. Lime Juice', '1/4 Oz. Agave or Simple Syrup (optional)', 'Garnishes: Salt Rim and Lime Wedge']
 	},
+	'recipe-4': {
+		name: 'Daquiri',
+		ingredients: ['1 1/2 Oz. White Rum', '3/4 Oz. Lime Juice',  '3/4 Oz. Simple Syrup']
+	},
+	'recipe-5': {
+		name: 'Whiskey Sour',
+		ingredients: ['2 Oz. Bourbon or Rye', '1 Oz. Lemon Juice',  '1 Oz. Simple Syrup', '1 Egg White (optional)']
+	},
+	'recipe-6': {
+		name: 'Tom Collins',
+		ingredients: ['1 1/2 Oz. Gin', '3/4 Oz. Lemon Juice', '3/4 Oz. Simple Syrup', 'Club Soda']
+	},
+	'recipe-7': {
+		name: 'Mai Tai',
+		ingredients: ['2 Oz. Dark Rum', '1 Oz. White Rum',  '3/4 Oz. Orange Curaçao', '3/4 Oz. Oregat Syrup', '1/2 Oz. Lime Juice', 'Garnishes: Orchid and Lime Wheel']
+	},
+	'recipe-8': {
+		name: 'Last Word',
+		ingredients: ['3/4 Oz. London Dry Gin', '3/4 Oz. Luxardo maraschino liqueur',  '3/4 Oz. green Chartreuse', '3/4 Oz. Lime Juice']
+	},
+
 }
 
 export default SampleRecipes;
